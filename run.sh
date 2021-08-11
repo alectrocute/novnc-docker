c3ff4a005d1de106320388d80b34427fa57690c2
alectrocute <alectrocute@gmail.com>
2021-08-11 09:56:41 -0400

    init

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+docker build . --tag orca
+docker rm -f orca || true
+docker run \
+  --detach \
+  --env DISPLAY_SETTINGS="1920x1080x24" \
+  --publish 8080:8080 \
+  --publish 8081:8081 \
+  --rm \
+  --name orca \
+  orca
+sleep 3
+open -a "Google Chrome" http://localhost:8080
